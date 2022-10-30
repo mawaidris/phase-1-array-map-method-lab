c9532df86acd7391ae8d9d981aca968d1ad62b73
mawa <mawahama05@gmail.com>
2022-10-29 22:31:59 -0700

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased(){
+  // const tutorials1 = tutorials.split(" ");
+ return tutorials.map( (tutorial) => {
+              //  return tutorial[0].toUpperCase() + tutorial.substring(0)
+              const cap = tutorial.split(" ");
+              const captutororial = cap.map( (e) =>{
+                return e.charAt(0).toUpperCase() + e.slice(1)
+              }).join(" ")
+              return captutororial
+})}
+
+console.log(titleCased());
+// const words = mySentence.split(" ");
+
+// words.map((word) => { 
+//     return word[0].toUpperCase() + word.substring(1); 
+// }).join(" ");
+
